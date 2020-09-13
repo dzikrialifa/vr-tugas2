7a7da70608dd7d6856f41ae28ac6c5c8627e57e2
D Dena Indah A <denaindah@gmail.com>
2020-09-13 10:43:15 +0700

    VRpertemuan2

---
+++ Assets/UbahWarna1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna1 : MonoBehaviour
+{
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Biru()
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    public void Merah()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/Ubahwarna2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ubahwarna2 : MonoBehaviour
+{
+    public void Biru()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Ungu()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
